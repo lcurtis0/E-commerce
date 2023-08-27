c57a10e00663752787423130db41f9d60586eaf4
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-08-27 16:40:24 -0400

    filled out index.js

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -2,11 +2,27 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    categoryname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        
+      }
+    }
   },
   {
     sequelize,
